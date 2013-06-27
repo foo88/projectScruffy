d7287b24b5401b8596456b9a68d0bfa6ee71f646
zettet <tzettersten@gmail.com>
2013-06-27 12:38:09 -0500

    created VS C# project and made sample class files

---
+++ Scruffy/Scruffy/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Class to manage an individual model. Should have a list of attributes, and a history of actions
+namespace Scruffy
+{
+    class Model
+    {
+    }
+}
